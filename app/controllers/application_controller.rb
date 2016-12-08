bb23175cfb71f68388023f964481c48d4b8dcbe7
Richard Zhang <lapislacrima@gmail.com>
2016-12-08 20:58:04 +0000

    added current_user, which sets the current_user instance variable when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
   private
 
   def cart
@@ -18,5 +23,8 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+
+
 
 end
