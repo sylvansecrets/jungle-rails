56276ab3670339685ed099914e9d9d649208a16d
Richard Zhang <lapislacrima@gmail.com>
2016-12-09 02:49:57 +0000

    converted username and password to .env

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
 
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
     @category = Category.order(id: :desc).all
