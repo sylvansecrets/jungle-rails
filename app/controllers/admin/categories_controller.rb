eb50f1bb4c16ef56e4b8a570d9ae13ba3fc90981
Richard Zhang <lapislacrima@gmail.com>
2016-12-09 01:49:50 +0000

    added basic HTTP authentication for all the admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: "Jungle", password: "book"
+
   def index
     @category = Category.order(id: :desc).all
   end
