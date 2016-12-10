0035b68355241d983d33ebae641e15aaf3626346
Richard Zhang <lapislacrima@gmail.com>
2016-12-10 02:56:04 +0000

    added destroy route for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resources :comments, only: [:create, :edit, :new]
+    resources :reviews, only: [:create, :edit, :new, :destroy]
   end
 
   resources :categories, only: [:show]
