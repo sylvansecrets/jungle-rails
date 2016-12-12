9a9e703e6e64e020368f831cd30efa7b2e732392
Richard Zhang <lapislacrima@gmail.com>
2016-12-12 23:21:59 +0000

    Add preferred_name and formatted_name columns to db/users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209200517) do
+ActiveRecord::Schema.define(version: 20161212222020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20161209200517) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "preferred_name"
+    t.string   "formatted_name"
   end
 
   add_foreign_key "line_items", "orders"
