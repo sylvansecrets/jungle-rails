c16a8a2354475ce8b3f4dd6e0d0c093123a08a61
Richard Zhang <lapislacrima@gmail.com>
2016-12-12 22:51:29 +0000

    added password length constraint \n added email uniqueness constraint \n added field for preferred_name \n added field for formatted_name \n added constraint for email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password
+
+  validates :email, uniqueness: true, case_sensitive: false
+  validates :email, presence: true
+  validates :preferred_name, presence: true
+  validates :formatted_name, presence: true
+  validates :password, length: {minimum: 6}
 end
